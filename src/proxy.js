88ee184398e37460a615916865f99c2fe5a630a7
Mohamed Ben Chamakh <mohamed.ben.chamakh98@gmail.com>
2024-02-26 19:01:44 +0100

    gitignore

--- src/proxy.js
+++ src/proxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: process.env.REACT_APP_SERVER_URI,
-            changeOrigin: true,
-        })
-    );
+module.exports = function(app) {
+  app.use(
+   '/api',
+    createProxyMiddleware({
+      target: 'https://flooka-tv-api.vercel.app/',
+      changeOrigin: true,
+    })
+  );
 };
